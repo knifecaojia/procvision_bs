df3880c8383a427ca0887e29eff75ab308b3780f
Mrggy <fortunatemen@163.com>
2026-09-13 17:31:04 +0800

    完善需求

--- src/backend/imustsz-business/src/main/java/com/imustsz/craft/domain/json/MaterialInfo.java
+++ src/backend/imustsz-business/src/main/java/com/imustsz/craft/domain/json/MaterialInfo.java
@@ -15,4 +15,10 @@ public class MaterialInfo {
     private String materialUnit;
     @JsonProperty("error_prevention_mark")
     private String errorPreventionMark;
+
+    //  以下为预留字段
+    private String assembly_number; // 装配序号
+    private String position_number; // 位号
+    private String model_no; // 型号
+    private String polarity_direction; // 极性方向
 }
